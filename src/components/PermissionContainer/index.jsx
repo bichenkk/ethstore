e0b932f1cdf6fd75d8b610f8b2e15abd1e56f9d0
KK Chen <kk@bichenkk.com>
2018-08-28 14:34:16 +0800

    Optimsie permission page

--- src/components/PermissionContainer/index.jsx
+++ src/components/PermissionContainer/index.jsx
@@ -1,7 +1,9 @@
 import React, { Component } from 'react'
+import { Card, Icon } from 'antd'
 import { drizzleConnect } from 'drizzle-react'
 import PropTypes from 'prop-types'
 import getContractMethodValue from '../../utils/getContractMethodValue'
+import './index.less'
 
 class PermissionContainer extends Component {
   constructor(props, context) {
@@ -21,7 +23,13 @@ class PermissionContainer extends Component {
     if (this.props.permission === 'storeOwner' && getIdentity.isStoreOwner) {
       return this.props.children
     }
-    return 'No Permission'
+    return (
+      <Card hoverable className='permission-card'>
+        <Icon style={{ fontSize: '28px', marginBottom: '24px' }} type='warning' />
+        <h1>You don&apos;t have enough permission to view this page.</h1>
+        <p>Please go back to the previous page.</p>
+      </Card>
+    )
   }
 }
 
